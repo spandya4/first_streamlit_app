3ddf221802ff6b8325785f2439395d22b0d6aa30
spandya4 <64553260+spandya4@users.noreply.github.com>
2023-06-27 16:05:30 -0400

    Update streamlit_app.py
    Added a new header.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-straemlit.title('My Parents New Healthy Diner')
+straemlit.title('My Moms New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale, Spinach & Recket Smothie')
 streamlit.text('Hard-boiled Free-Range Eggs')
+streamlit.text('🥑🍞Avacado toast')
+
+streamlit.header('Build your own fruit smoothie ')
