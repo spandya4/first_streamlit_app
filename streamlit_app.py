216c446a23cc1362f1dd3481c47a933a16ee154b
spandya4 <64553260+spandya4@users.noreply.github.com>
2023-06-28 09:30:06 -0400

    Update streamlit_app.py
    Added pandas frame.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('Hard-boiled Free-Range Eggs')
 streamlit.text('🥑🍞Avacado toast')
 
 streamlit.header('Build your own fruit smoothie ')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+streamlit.dataframe(my_fruit_list)
