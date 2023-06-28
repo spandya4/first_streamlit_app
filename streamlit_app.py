a91d9986dcb8019a72e70736ae6394fd1ebc3579
spandya4 <64553260+spandya4@users.noreply.github.com>
2023-06-28 09:36:35 -0400

    Update streamlit_app.py
    fixed a missing ')'

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ streamlit.text('🥑🍞Avacado toast')
 streamlit.header('Build your own fruit smoothie ')
 
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
