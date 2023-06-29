4c1c7042355ab25cd2ca28317a8d5db2aa4800fc
spandya4 <64553260+spandya4@users.noreply.github.com>
2023-06-29 17:19:23 -0400

    Update streamlit_app.py
    Apple

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Apple")
 streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
 streamlit.write('The user entered ', fruit_choice)
 
 # write your own comment -what does the next line do? 
