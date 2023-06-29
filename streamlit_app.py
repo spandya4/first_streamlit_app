7a3c6b20eef12d36e0c490fc9141ff4c0176957f
spandya4 <64553260+spandya4@users.noreply.github.com>
2023-06-29 16:49:51 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,5 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.jason())
+streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
