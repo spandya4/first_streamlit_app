9a4f6c845db0d20c19141bb8c1be5de8f88653e6
spandya4 <64553260+spandya4@users.noreply.github.com>
2023-06-30 16:30:00 -0400

    Update streamlit_app.py
    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from utillib.error import URLError
+from urllib.error import URLError
 
 streamlit.title('My Moms New Healthy Diner')
 
